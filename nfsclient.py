ae28c71dbb7c6a60b6d39825b516845933a7fb57
Ajay Pandurang Paratmandali <ajaypp123@gmail.com>
2019-02-06 11:50:13 +0530

    Optimise nfs client

--- nfsclient.py
+++ nfsclient.py
@@ -3,11 +3,18 @@
 import os
 import sys
 
-a=sys.argv[1]
-#a=raw_input('enter the path and file name at client side\n')
+def usage():
+        return "nfs-client.py <shared-dir-path> <nfs-server-hostname>"
+
+if sys.argv[1] == "-h":
+        print usage()
+        sys.exit(1)
+else:
+        a=sys.argv[1]
+
 os.system('yum install -y nfs-utils nfs-utils-lib')
-os.system('mkdir '+a)
+os.system('mkdir '+ a)
 file=open("/etc/fstab",'a')
-file.write("\nnfsserver:/var/data\t"+a+"\tnfs\tdefaults\t0\t0")
+file.write("\n" + sys.argv[2] + ":/var/data\t"+a+"\tnfs\tdefaults\t0\t0")
 file.close()
 os.system('mount -a')
