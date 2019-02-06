cf4a750198fe2d9fa2934ad12aa899067671838f
Ajay Pandurang Paratmandali <ajaypp123@gmail.com>
2019-02-06 10:53:39 +0530

    Optimization

--- nfsserver.py
+++ nfsserver.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 import os
-a=raw_input("Enter the path name of folder to be shared")
+import sys
+
+a=sys.argv[1]
+#a=raw_input("Enter the path name of folder to be shared")
 os.system('mkdir '+a)
-data=a+"\t*.example.com(rw,sync)"
+data=a+"\t*(rw,sync)"
 file=open("/etc/exports",'w')
 file.write(data)
 file.close()
-os.system('yum install nfs* -y')
+os.system('yum install nfs-utils rpcbind nfs-utils-lib -y')
 os.system('systemctl restart nfs')
 os.system('systemctl restart nfs-server.service')
 os.system('showmount -e')
 os.system('firewall-cmd --permanent --add-service=nfs')
 os.system('firewall-cmd --reload')
 os.system('chmod 777 '+a)
-~                         
