9fb1786def383bca5d8e2f5adde738aa9c1e0bfb
Ajay Pandurang Paratmandali <ajaypp123@gmail.com>
2019-02-06 11:36:41 +0530

    Update nfsclient.py

--- nfsclient.py
+++ nfsclient.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
+
 import os
-a=raw_input('enter the path and file name at client side\n')
+import sys
+
+a=sys.argv[1]
+#a=raw_input('enter the path and file name at client side\n')
+os.system('yum install -y nfs-utils nfs-utils-lib')
 os.system('mkdir '+a)
 file=open("/etc/fstab",'a')
-file.write("\nserver14:/nfsfile\t"+a+"\tnfs\tdefaults\t0\t0")
+file.write("\nnfsserver:/var/data\t"+a+"\tnfs\tdefaults\t0\t0")
 file.close()
 os.system('mount -a')
-
